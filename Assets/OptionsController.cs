2726e5a502f49b22e77ae641d9b600b87dace039
Yihao Yang <yihaoyan@usc.edu>
2018-06-06 14:05:14 -0400

    Animation

---
+++ Assets/OptionsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour {
+
+    public Slider volumeSlider;
+    public LevelManager levelManager;
+    public Slider difficultySlider;
+
+    private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+        musicManager = GameObject.FindObjectOfType<MusicManager>();
+        difficultySlider.value = PlayerPrefManager.GetDifficulty();
+        volumeSlider.value = PlayerPrefManager.getMasterVolume();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        musicManager.ChangeVolume(volumeSlider.value);
+	}
+
+    public void SaveAndExit(){
+        PlayerPrefManager.SetMasterVolume(volumeSlider.value);
+        PlayerPrefManager.SetDifficulty(difficultySlider.value);
+        levelManager.LoadLevel("01a Start");
+    }
+
+    public void SetDefaults(){
+        volumeSlider.value = 0.8f;
+        difficultySlider.value = 2f;
+    }
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+    }
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    private void OnLevelWasLoaded(int level)
+    {
+        AudioClip audioClip = levelMusicChangeArray[level];
+        Debug.Log("Playing clip: " + audioClip);
+        if (audioClip){
+            audioSource.clip = audioClip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+    public void ChangeVolume(float vol){
+        audioSource.volume = vol;
+    }
+}
